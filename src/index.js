02d46cdff0c8dd5b112eeac9d8717c73a63039a6
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-08-31 22:03:10 +0300

    fixed basename for drowserouter

--- src/index.js
+++ src/index.js
@@ -7,10 +7,8 @@ import './shared/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <BrowserRouter  basename={"/goit-react-hw-05-movies"}>
+    <BrowserRouter basename={"goit-react-hw-05-movies"}>
       <App />
-      </BrowserRouter>
     </BrowserRouter>
   </React.StrictMode>
 );
