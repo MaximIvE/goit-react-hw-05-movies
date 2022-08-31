ce96bb15a30e76f5b119e11839ebf0ee268de4cd
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-08-31 21:55:18 +0300

    add browserouter

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import './shared/styles/index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
+    <BrowserRouter  basename={"/goit-react-hw-05-movies"}>
       <App />
+      </BrowserRouter>
     </BrowserRouter>
   </React.StrictMode>
 );
