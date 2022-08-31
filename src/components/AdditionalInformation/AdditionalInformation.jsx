72cb82eb59bb949cda58c0f6eb8be77b90c95194
Maksym Ivaniuk <ivanukmaxim@gmail.com>
2022-08-31 22:52:31 +0300

    fixed button Go back

--- src/components/AdditionalInformation/AdditionalInformation.jsx
+++ src/components/AdditionalInformation/AdditionalInformation.jsx
@@ -3,15 +3,16 @@ import PropTypes from 'prop-types';
 import {Wrapper, LinkSt} from './AdditionalInformation.styled';
 
 const AdditionalInformation = ({id}) => {
-    const location =useLocation();
+    const location = useLocation();
     const back = location.state?.from || "/";
+    
     
     return (
         <Wrapper>
             <h3>Additional information</h3>
             <ul>
-                <li key="cast"   ><LinkSt state={{back}} to={`/movies/${id}/cast`}   >Cast   </LinkSt></li>
-                <li key="reviews"><LinkSt state={{back}} to={`/movies/${id}/reviews`}>Reviews</LinkSt></li>
+                <li key="cast"   ><LinkSt state={{from: back}} to={`/movies/${id}/cast`}   >Cast   </LinkSt></li>
+                <li key="reviews"><LinkSt state={{from: back}} to={`/movies/${id}/reviews`}>Reviews</LinkSt></li>
             </ul>
         </Wrapper>
     )
